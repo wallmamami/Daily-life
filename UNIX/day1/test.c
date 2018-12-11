45aeee892d2f1cc5f971ddd33b7a4a4d04c2e9bf
Centos7.0 <wj@localhost.localdomain>
2018-12-11 01:20:34 -0800

    add

--- UNIX/day1/test.c
+++ UNIX/day1/test.c
@@ -1,1 +1,32 @@
-#include "apue.h"
+#include <unistd.h>
+#include <stdio.h>
+
+#define BUFFSIZE 4096
+
+//使用 ./a.out < infile > outfile
+//可以将infile中的文件拷贝到outfile
+
+int main()
+{
+  int n;
+  char buf[BUFFSIZE];
+
+  //这里为什么可以一直读
+  //read默认为阻塞接口，
+  //假如一直读，这里不是应该会阻塞吗?
+  //这里的STDIN_FILENO与STDOUT_FILENO
+  //为两个宏,指定了标准输入和标准输出的fd
+  while((n = read(STDIN_FILENO , buf, BUFFSIZE)) > 0)
+  {
+    if(write(STDOUT_FILENO, buf, n) != n)
+    {
+      perror("write");
+    }
+  }
+
+  if(n < 0)
+    perror("read");
+
+  return 0;
+}
+
